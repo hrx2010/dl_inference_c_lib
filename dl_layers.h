0b132e0cca8562a784978331fd727d775cb5d37e
hrx2010 <mark.wangzhe@gmail.com>
2019-07-26 17:40:45 +0800

    fixed-point 1D conv

--- dl_layers.h
+++ dl_layers.h
@@ -5,55 +5,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-// define structure of weights
-struct cls_tensor_weights{
-    int kernal_height, kernal_width, kernal_depth;
-    int num_filters;
-    char *filters;
-};
-
-// define structure of biases
-struct cls_tensor_biases{
-    int kernal_depth;
-    char *biases;
-};
-
-// define strcucture of activations
-struct cls_tensor_activations{
-    int height, width, depth;
-    char *feature_map;
-};
 
 struct cls_tensor_weights_1D{
     int num_filters , num_rows , num_cols;
-    float ***filters;
+    char ***filters;
 };
 
 struct cls_tensor_activations_1D{
     int num_rows , num_cols;
-    float **feature_map;
+    char **feature_map;
 };
 
-// compute tensor size after convolutional layer
-int calc_dim_conv_layer(int original , int zero_padding , int size_filter , int stride);
+
+// load data from file
 
 int compute_output_cols_convolution_1D(int num_input_col , int num_weight_col);
 
-// load data from file
-struct cls_tensor_activations convolution_2D(struct cls_tensor_activations input , struct cls_tensor_weights weights , struct cls_tensor_biases biases, int zero_padding , int stride);
-
-struct cls_tensor_weights read_weights_from_file(char *filename , int kernal_height , int kernal_width , int kernal_depth , int num_filters);
-
-struct cls_tensor_biases read_biases_from_file(char *filename , int kernal_depth);
-
-struct cls_tensor_weights_1D read_weights_1D_from_file(char *filename , int num_filters , int num_rows , int num_cols);
-
-struct cls_tensor_activations read_activations_from_file(char *filename , int height , int width , int depth);
-
-struct cls_tensor_activations_1D read_activations_1D_from_file(char *filename , int num_rows , int num_cols);
-
 struct cls_tensor_activations_1D convolution_1D_no_padding(struct cls_tensor_activations_1D input , struct cls_tensor_weights_1D weights);
 
+struct cls_tensor_activations_1D read_activations_from_source_code(char *data , int num_rows , int num_cols);
+
+struct cls_tensor_weights_1D read_weights_1D_from_source_code(char *data , int num_filters , int num_rows , int num_cols);
 
 // compute mean square error
 float compute_mean_square_error(struct cls_tensor_activations output , struct cls_tensor_activations ground_truth);
